3f7ea8ef5cf0a1436daea2986f29abaca357cb6d
João Ramos <jhramos1406@gmail.com>
2026-08-21 21:55:11 -0300

    CRUD

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,63 @@
 import express from "express";
+import { randomUUID } from "node:crypto";
 
 const app = express();
+app.use(express.json());
+
+//=======================
+// Initial data
+//=======================
+const pizzaCategoryId = randomUUID();
+const drinksCategoryId = randomUUID();
+
 
 const categories = [
-
-[
   {
-    "id": 1,
-    "name": "Pizzas Tradicionais",
+    "id": pizzaCategoryId,
+    "name": "Pizzas",
     "description": "Pizzas com sabores clássicos, preparadas com ingredientes tradicionais."
   },
   {
-    "id": 2,
-    "name": "Pizzas Especiais",
-    "description": "Pizzas com combinações exclusivas e ingredientes selecionados."
-  },
-  {
-    "id": 3,
+    "id": drinksCategoryId,
     "name": "Bebidas",
     "description": "Refrigerantes, sucos, águas e outras bebidas para acompanhar o pedido."
   }
-]]
+]
 
 const products = [
-    [
   {
-    "id": 1,
-    "categoryID": 1,
+    "id": randomUUID(),
+    "categoryID": pizzaCategoryId,
     "name": "Pizza Margherita",
     "description": "Molho de tomate, mussarela, tomate fresco, manjericão e azeite.",
     "price": 49.90
   },
   {
-    "id": 2,
-    "categoryID": 1,
+    "id": randomUUID(),
+    "categoryID": pizzaCategoryId,
     "name": "Pizza Calabresa",
     "description": "Molho de tomate, mussarela, calabresa fatiada e cebola.",
     "price": 54.90
   },
   {
-    "id": 3,
-    "categoryID": 2,
+    "id": randomUUID(),
+    "categoryID": pizzaCategoryId,
     "name": "Pizza Frango com Catupiry",
     "description": "Molho de tomate, mussarela, frango desfiado e Catupiry.",
     "price": 62.90
   },
   {
-    "id": 4,
-    "categoryID": 3,
+    "id": randomUUID(),
+    "categoryID": drinksCategoryId,
     "name": "Coca-Cola 2L",
     "description": "Refrigerante Coca-Cola 2 litros.",
     "price": 15.90
   }
-]]
+]
 
-
-
+//=======================
+// Root
+//=======================
 app.get("/", (req, res) => {
     res.status(200).json({
         message: "API Restaurante",
@@ -63,12 +65,66 @@ app.get("/", (req, res) => {
     });
 });
 
+//=======================
+// Categorias
+//=======================
 app.get("/categories",(req, res) => {
     res.status(200).json(categories);
 });
 
+app.get("/categories/:id",(req, res) => {
+    const category = categories.find((category) => {
+      return category.id == req.params.id;
+    });
+
+    if(!category) {
+      return res.status(404).json({
+        message: "Categoria não encontrada."
+      });
+    }
+
+    res.status(200).json(category);
+});
+
+app.post("/categories", (req, res) => {
+
+  const category = {
+    id: randomUUID(),
+    ...req.body,
+  }
+  categories.push(category);
+  res.status(201).json(category)
+
+})
+
+//=======================
+// Produtos
+//=======================
 app.get("/products",(req, res) => {
     res.status(200).json(products);
 });
+
+app.get("/products/:id",(req, res) => {
+    const product = products.find((product) => {
+      return product.id == req.params.id;
+    });
+
+    if(!product) {
+      return res.status(404).json({
+        message: "Produto não encontrada."
+      });
+    }
+
+    res.status(200).json(product);
+});
+
+app.post("/products", (req, res) => {
+  const product = req.body;
+
+  products.push(product);
+
+  res.status(201).json(product)
+
+})
 
 export default app; 
